2299cf0956305bfc3b8e4f3409c1f6c5f769d0d0
stangacatalin <ionut.stanga@s.unibuc.ro>
2021-05-15 01:13:57 +0300

    First piece prototype

--- Chess.xaml.cs
+++ Chess.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using GameHub.ChessClasses;
 namespace GameHub
 {
     /// <summary>
@@ -23,6 +11,7 @@ namespace GameHub
         public Chess()
         {
             InitializeComponent();
+            Piece.addPiece(gridTable);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
---
+++ ChessClasses/Piece.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+
+namespace GameHub.ChessClasses
+{
+    public class Piece
+    {
+        int currentx, currenty;
+        private static BitmapFrame changeBtnBckg(string imageName)
+        {
+            Uri resourceUri = new Uri("Images/" + imageName + ".png", UriKind.Relative);
+            StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+
+            BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            
+            
+            return temp;
+            
+        }
+        public static void addPiece(Grid grid)
+        {
+            Button btnRook = new Button();
+            ImageBrush brush = new ImageBrush(changeBtnBckg("BlackRook"));
+            btnRook.Background = brush;
+            Grid.SetColumn(btnRook, 0);
+            Grid.SetRow(btnRook, 0);
+            grid.Children.Add(btnRook);
+        }
+    }
+}
